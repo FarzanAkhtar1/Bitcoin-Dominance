c2829d722f8adefe26dfbffa30a98065cc1c38a1
FarzanAkhtar1 <47488690+FarzanAkhtar1@users.noreply.github.com>
2021-05-12 01:39:44 +0100

    Tidy Code + Update README
    Tidied some of the code
    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Bitcoin-Dominance
-Caclualtes Bitcoin Dominance using Coingecko's API and writes it to a CSV along with the date
+Caculuates Bitcoin Dominance using Coingecko's API and writes it to a CSV along with the date.
+The dominance is calculated out of 100 by comparing the market cap of Bitcoin to the market cap of all other cryptocurrencies.
+Dominance is used to measure the growth of alt coins compared to Bitcoin:
+    Falling dominanace over time -> Alt coins could be growing more than Bitcoin
+    Rising dominance over time -> Bitcoin could be growing more than alt coins
+
+Requirements:
+- Requests Library
+
+Once requirements are installed, this can be run directly.
+
+Configuartion:
+By default, the date is in the format dd-mm-yyyy
+To add a timestamp, comment out Line 15, and uncomment Line 16. This will write the date in the format dd-mm-yyyy HH:MM:SS
+
+
--- btcdom.py
+++ btcdom.py
@@ -1,23 +1,30 @@
+import csv
 import json
-import requests
 import time
 from datetime import datetime
-import csv
 
+import requests
+
+#gets data from coingecko
 response = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false')
-#print(response)
 response = response.json()
+
+#initialises variables
 BTCCap = 0
 altCap = 0
+current_time = datetime.now().strftime("%d-%m-%Y")
+#current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+
+#iterates through response
 for x in response:
     #print(x['id'])
-    if x['id'] == "bitcoin":
+    if x['id'] == "bitcoin": #adds bitcoin market cap to BTCCap and altCap
         BTCCap = x['market_cap']
         altCap = altCap + x['market_cap']
-    else:
+    else: #adds any altcoin market cap to altCap
         altCap = altCap + x['market_cap']
         
-current_time = datetime.now().strftime("%d-%m-%Y")
-with open("BTCDominance.csv", mode='a+', newline ="") as datawriter:
+with open("BTCDominance.csv", mode='a+', newline ="") as datawriter: #writes the data to a CSV BTCDominance, will generate the file if it does not exist.
     datawriter = csv.writer(datawriter, delimiter=",")
-    datawriter.writerow([current_time,"{:.2f}".format((BTCCap/altCap)*100)])
+    datawriter.writerow([current_time,"{:.2f}".format((BTCCap/altCap)*100)]) #writes bitcoin dominance out of 100 to 2dp
